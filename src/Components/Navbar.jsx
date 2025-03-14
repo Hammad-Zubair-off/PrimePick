9951ba8ec34e54aea62fd2a71312459b81b913e4
Hammad Zubair <hammadzubairofficial@gmail.com>
2025-03-14 12:16:28 +0500

    Update Navbar code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useContext, useState } from "react";
-import { ShopContext } from "../Context/ShopContext";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
@@ -13,25 +13,17 @@ const Navbar = () => {
         <img src={assets.logo} alt="logo" className="w-36" />
       </Link>
 
+      {/* Navigation Links */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-        <NavLink to="/" className="flex flex-col items-center gap-1">
-          <p>HOME</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-        </NavLink>
-        <NavLink to="/collection" className="flex flex-col items-center gap-1">
-          <p>COLLECTION</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-        </NavLink>
-        <NavLink to="/about" className="flex flex-col items-center gap-1">
-          <p>ABOUT</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-        </NavLink>
-        <NavLink to="/contact" className="flex flex-col items-center gap-1">
-          <p>CONTACT</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-        </NavLink>
+        {["home", "collection", "about", "contact"].map((path) => (
+          <NavLink key={path} to={`/${path}`} className="flex flex-col items-center gap-1">
+            <p>{path.toUpperCase()}</p>
+            <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+          </NavLink>
+        ))}
       </ul>
 
+      {/* Icons Section */}
       <div className="flex items-center gap-6">
         <img
           onClick={() => setShowSearch(true)}
@@ -39,11 +31,9 @@ const Navbar = () => {
           alt="search"
           className="w-5 cursor-pointer"
         />
-        <div className="group relative">
-          <Link to="/login">
-            <img src={assets.profile_icon} alt="profile" className="w-5 cursor-pointer" />
-          </Link>
-        </div>
+        <Link to="/login">
+          <img src={assets.profile_icon} alt="profile" className="w-5 cursor-pointer" />
+        </Link>
 
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} alt="cart" className="w-5 min-w-5" />
@@ -52,6 +42,7 @@ const Navbar = () => {
           </p>
         </Link>
 
+        {/* Mobile Menu Icon */}
         <img
           src={assets.menu_icon}
           alt="menu"
@@ -59,31 +50,29 @@ const Navbar = () => {
           onClick={() => setVisible(!visible)}
         />
       </div>
-      
 
+      {/* Sidebar for Mobile */}
       <div
-        className={`sidebar absolute top-0 right-0 bottom-0 overflow-hidden bg-white ease-in duration-300 ${
-          visible ? "w-full" : "w-0"
+        className={`sidebar fixed top-0 right-0 bottom-0 bg-white shadow-lg transition-transform duration-300 ${
+          visible ? "translate-x-0 w-64" : "translate-x-full w-0"
         }`}
       >
         <div className="flex flex-col text-gray-600">
-          <div onClick={() => setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+          <button onClick={() => setVisible(false)} className="flex items-center gap-4 p-3">
             <img src={assets.dropdown_icon} alt="close" className="h-4 rotate-180" />
             <p className="font-semibold">Back</p>
-          </div>
+          </button>
 
-          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/">
-            HOME
-          </NavLink>
-          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/collection">
-            COLLECTION
-          </NavLink>
-          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/about">
-            ABOUT
-          </NavLink>
-          <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border" to="/contact">
-            CONTACT
-          </NavLink>
+          {["home", "collection", "about", "contact"].map((path) => (
+            <NavLink
+              key={path}
+              onClick={() => setVisible(false)}
+              className="py-2 pl-6 border"
+              to={`/${path}`}
+            >
+              {path.toUpperCase()}
+            </NavLink>
+          ))}
         </div>
       </div>
     </div>
